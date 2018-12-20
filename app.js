c05be1f228f3b91a3ba98f91e7697b901049b026
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-20 15:48:46 +0000

    Calculate and error functions

--- app.js
+++ app.js
@@ -20,10 +20,48 @@ function calculateResults(e) {
 
     // Compute monthly payment
     const x = Math.pow(1 + calculateInterest, calculatePayments);
+    const monthly = (principal * x * calculateInterest) / (x - 1);
 
+    // is finite
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatePayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatePayments) - principal).toFixed(2);
+    } else {
+        // console.log('Please check your numbers');
+        showError('Please check your numberes');
 
-
+    }
     // prevent default behavior
     e.preventDefault();
 
+}
+
+
+// Show Error
+function showError(error) {
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    // Before heading
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading 
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    // 3sec
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
 }
